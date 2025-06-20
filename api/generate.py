ef3ae828839cf0ab2e28024e7bf1b8d366ec56b4
Call Me AL <116076493+xenzofficial@users.noreply.github.com>
2025-06-20 21:05:09 +0700

    Update generate.py

--- api/generate.py
+++ api/generate.py
@@ -21,6 +21,9 @@ cookies = {
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         
